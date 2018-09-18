8aca0311c845dbd70d3c77f8ee8da823a26ce002
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-18 20:34:34 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
-def square_array(array)
-  # your code here
+# def square_array(array)
+# end
+
+def square_array(numbers)
+  numbers = [1,2,3]
+  
+end
+
+brothers = ["Tim", "Tom", "Jim"]
+counter = 1
+brothers.each do |brother|
+  puts "This is loop number #{counter}"
+  puts "Stop hitting yourself #{brother}!"
+  counter += 1
 end
